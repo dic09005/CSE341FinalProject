33d75f5630e485660cda34bfe33720a3e3a10a18
JDeVoldemort <104032188+JDeVoldemort@users.noreply.github.com>
2023-07-11 15:06:24 -0600

    testing implemented for get

--- app.js
+++ app.js
@@ -7,21 +7,21 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 
-// const { auth, requiresAuth } = require('express-openid-connect');
+const { auth, requiresAuth } = require('express-openid-connect');
 
-// const config = {
-//   authRequired: false,
-//   auth0Logout: true,
-//   secret: process.env.SECRET,
-//   baseURL: process.env.BASE_URL,
-//   clientID: process.env.CLIENT_ID,
-//   issuerBaseURL: process.env.ISSUER_BASE_URL,
-// };
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
 
 // auth router attaches /login, /logout, and /callback routes to the baseURL
 // app.use(auth(config));
 
-// req.isAuthenticated is provided from the auth router
+// // req.isAuthenticated is provided from the auth router
 // app.get('/', (req, res) => {
 //   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 // });
@@ -47,6 +47,7 @@ mongodb.initDb((err) => {
     console.log(err);
   } else {
     app.listen(port);
-    console.log(`Connected to DB and listening on ${port}`);
+    // console.log(`Connected to DB and listening on ${port}`);
   }
 });
+module.exports = app;
